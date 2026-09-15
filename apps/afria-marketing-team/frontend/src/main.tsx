e044fc671834d02ec802dc1e084364e471ec51ff
lanickbusiness1 <lanickbusiness1@gmail.com>
2026-09-15 03:57:56 +0000

    feat: route /start to capability entry view

--- apps/afria-marketing-team/frontend/src/main.tsx
+++ apps/afria-marketing-team/frontend/src/main.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import StartEntryView from "./StartEntryView";
+import { isStartEntryPath } from "./startEntry";
+
+const RootView = isStartEntryPath(window.location.pathname) ? StartEntryView : App;
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RootView />
   </React.StrictMode>
 );
